e8c57ff502854a1da74be2dc6a51eb5a93b081f2
Aaron ("AJ") Steers <aj@meltano.com>
2023-05-04 15:42:09 -0700

    cleanup reverse el flows

--- samples/gh_analytics/stack/stack.py
+++ samples/gh_analytics/stack/stack.py
@@ -1,8 +1,7 @@
-import typing as t
-
-from simpler import flows, naming, stack
+from simpler import naming, stack
 from simpler.interop import duckdb
 
+from . import reverse_el
 from .sources import github, jaffle_shop
 
 WAREHOUSE_DB_PATH = "./db.duckdb"
@@ -24,4 +23,7 @@ class GitHubStack(stack.DataStack):
     storage_scheme = duckdb.DuckDBDWStorageScheme(path=WAREHOUSE_DB_PATH)
 
     # Reverse EL flows that should run after the DW is ready.
-    output_flows = t.Iterable[flows.ELDataFlow]
+    output_flows = [
+        reverse_el.GitHubReverseEL(),
+        reverse_el.SlackReverseEL(),
+    ]
